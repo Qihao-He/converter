44560826833ae14893abf54c103c1081b6a73d7d
Qihao He <qi.he@maine.edu>
2019-08-07 12:00:18 -0700

    Reading File Contents

--- lib/src/converter.dart
+++ lib/src/converter.dart
@@ -2,5 +2,6 @@ import 'dart:io';
 import 'package:image/image.dart';
 
 convertImage(FileSystemEntity selectedFile, String format) {
-  
+  final rawImage = (selectedFile as File).readAsBytesSync();
+  final image = decodeImage(rawImage);
 }

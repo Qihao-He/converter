9d886459c1865a4fe03a9512d443b0cb7928380a
Qihao He <qi.he@maine.edu>
2019-08-06 23:41:21 -0700

    Forcibly Exiting a Program

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
 import 'package:prompter_qh/prompter_qh.dart';
 
 void main() {
   final prompter = new Prompter();
 
-  prompter.askBinary('Are you here to convert an image?');
+  final bool choice = prompter.askBinary('Are you here to convert an image?');
+  if(!choice) {
+    exit(0);
+  }
 }
